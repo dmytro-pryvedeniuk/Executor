4ebf618ef3aabcb2689e3079094076b3304638e2
Dmytro Pryvedeniuk <dmytro.pryvedeniuk@gmail.com>
2018-04-28 00:24:48 +0300

    Removed the empty method, moved ObjectNotDisposedException to the separate file

--- Executor/Executor.cs
+++ Executor/Executor.cs
@@ -5,17 +5,6 @@ using System.Threading;
 namespace Executor
 {
     /// <summary>
-    /// Represents error which occur when the object of class implementing <see cref="IDisposable"/>
-    /// is not disposed properly.
-    /// </summary>
-    public class ObjectNotDisposedException : Exception
-    {
-        public ObjectNotDisposedException(string message): base(message)
-        {
-        }
-    }
-
-    /// <summary>
     /// Executes the tasks at any time by any number of clients.
     /// </summary>
     public sealed class Executor : IDisposable
---
+++ Executor/ObjectNotDisposedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Executor
+{
+    /// <summary>
+    /// Represents error which occur when the object of class implementing <see cref="IDisposable"/>
+    /// is not disposed properly.
+    /// </summary>
+    public class ObjectNotDisposedException : Exception
+    {
+        public ObjectNotDisposedException(string message): base(message)
+        {
+        }
+    }
+}
